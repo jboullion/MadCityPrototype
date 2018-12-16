5d2101366fbbc9e90150c1d915b7bf7a235d0da8
jboullion <jboullion83@gmail.com>
2018-12-16 03:09:30 -0600

    Testing live remove

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once('includes/setup.php');
 <!doctype html>
 <html lang="en">
 	<?php require_once('templates/common/header.php'); ?>
-	<body>
+	<body style="background: red;">
 		<?php require_once('templates/common/navigation.php'); ?>
 		<?php require_once('templates/character/sheet.php'); ?>
 		<?php require_once('templates/character/actions/actions.php'); ?>
